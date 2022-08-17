eaaf87b6b970f71df424ee44b315c55334949def
Aniket <aniketmahure143@gmail.com>
2022-08-17 22:45:42 +0530

    remove unused files

--- src/MERCHANT_MODULE/pages/products/Addproduct.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Addproduct = () => {
-  return <div>Addproduct</div>;
-};
-
-export default Addproduct;
--- src/MERCHANT_MODULE/pages/products/ViewProducts.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ViewProducts = () => {
-  return <div>ViewProducts</div>;
-};
-
-export default ViewProducts;
